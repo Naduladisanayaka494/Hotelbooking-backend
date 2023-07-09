66201a8e92b0ee296a70ffae9edf70d45bf0aee9
nadula.fourdiv <nadula.fourdiv@gmail.com>
2023-07-09 22:00:15 +0530

    single room

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express= require("express");
 const app= express();
 const dbconfig =require('./db')
 const roomsRoute =require('./routes/roomsRoute');
+app.use(express.json());
 app.use('/api/rooms', roomsRoute);
 const port= process.env.PORT||5000;
 
