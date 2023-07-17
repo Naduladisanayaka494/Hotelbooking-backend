da74618f4f6b9762a6018373409412c61d559a66
nadula.fourdiv <nadula.fourdiv@gmail.com>
2023-07-17 21:40:54 +0530

    cancell bookings API

--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -55,4 +55,24 @@ router.post("/getbookingsbyuserid",async(req,res)=>{
     
   }
 });
+
+router.post("/cancelbooking",async(req,res)=>{
+  const {bookingid,roomid}=req.body
+  try {
+    const bookingitem = await Booking.findOne({_id:bookingid})
+    bookingitem.status ='cancelled'
+    await bookingitem.save()
+    const room = await Room.findOne({_id:roomid})
+    console.log(room)
+    const bookings =room.currentbookings
+    const temp = bookings.filter(booking=>booking.bookingid.toString()!==bookingid)
+    room.currentbookings=temp
+    await room.save();
+    res.send('Your booking cancelled successfully')
+    
+  } catch (error) {
+    return res.status(400).json({error});
+    
+  }
+});
 module.exports=router;
